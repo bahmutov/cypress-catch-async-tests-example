8e29795144f5745c3b63f4d09e8d7a7b266b6a05
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-09-09 07:27:50 -0400

    initial example

---
+++ README.md
@@ -0,0 +1,5 @@
+# cypress-catch-async-tests-example
+
+> Catching accidental "async" test callbacks example
+
+Uses plugin [cypress-catch-async-tests](https://github.com/bahmutov/cypress-catch-async-tests)
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,16 @@
+it('sync callback', () => {
+  cy.wrap(42).should('equal', 42)
+})
+
+// https://github.com/cypress-io/cypress/issues/4742
+function getBar() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('bar')
+    }, 1000)
+  })
+}
+
+it('async callback', async () => {
+  cy.contains(await getBar())
+})
